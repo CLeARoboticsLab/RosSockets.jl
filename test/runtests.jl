3c8ae87d432852f8b729fd77b401c33a74241260
jake-levy <jake.j.levy@gmail.com>
2022-12-31 11:22:01 -0600

    velocity_control test complete

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,11 @@
-using RosSockets, Sockets
-using Test
+using Test, RosSockets, Sockets
+import JSON
 
 function test_velocity_control()
-    # create a server on localhost that listens for a connection and reads data
+    commands = [[1.2,3.4], [5.6,7.8]]
+    
+    # create a server on localhost that listens for a connection, reads data,
+    # and puts the data on a channel
     ip = "127.0.0.1"
     port = 42450
     channel = Channel(1)
@@ -16,11 +19,13 @@ function test_velocity_control()
 
     # connect to localhost, send data, then close the connection
     robot_connection = open_robot_connection(ip, port)
-    send_control_commands(robot_connection, [[1.2,3.4]])
+    send_control_commands(robot_connection, commands)
     close_robot_connection(robot_connection, stop_robot=false)
+
+    # take the data from the channel and compare it to the sent data
     received_payload = take!(channel)
-    println(received_payload)
-    @test 3 == 3 # TODO
+    data = JSON.parse(received_payload)
+    @test data["controls"] == commands
 end
 
 @testset "RosSockets.jl" begin
